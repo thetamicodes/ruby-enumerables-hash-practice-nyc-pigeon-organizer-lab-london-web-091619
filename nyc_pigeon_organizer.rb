fb9f3564d5e0e6cbe859731abe39965232588817
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 19:44:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,8 +18,7 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-
+pigeon_list = {}
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
